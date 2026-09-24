b1c528bb4177117a6cddcbf7b7d16182eee59a47
KianChesterBautista <kianchesterbautista@gmail.com>
2026-09-24 17:32:32 +0800

    Update initialize.php

--- api/initialize.php
+++ api/initialize.php
@@ -5,11 +5,11 @@ $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 $db_name = getenv('DB_NAME');
 
-$conn = mysqli_init();
-$conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+$connection = mysqli_init();
+$connection->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 
 try {
-    $conn->real_connect($host, $user, $pass, $db_name, $port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+    $connection->real_connect($host, $user, $pass, $db_name, $port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 } catch (mysqli_sql_exception $e) {
     die("Database Connection Error: " . $e->getMessage());
 }
