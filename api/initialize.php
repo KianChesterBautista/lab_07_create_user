be6d8a49386d2259c391159ab47d6e6653cc2520
KianChesterBautista <kianchesterbautista@gmail.com>
2026-09-24 17:18:24 +0800

    Refactor database connection variables and initialization

--- api/initialize.php
+++ api/initialize.php
@@ -1,15 +1,15 @@
 <?php
-$db_host = getenv('DB_HOST');
-$db_port = getenv('DB_PORT') ?: 3306;
-$db_user = getenv('DB_USER');
-$db_pass = getenv('DB_PASS');
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT') ?: 3306;
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 $db_name = getenv('DB_NAME');
 
-$db = mysqli_init();
-$db->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+$conn = mysqli_init();
+$conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 
 try {
-    $db->real_connect($db_host, $db_user, $db_pass, $db_name, $db_port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+    $conn->real_connect($host, $user, $pass, $db_name, $port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 } catch (mysqli_sql_exception $e) {
     die("Database Connection Error: " . $e->getMessage());
 }
