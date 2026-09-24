921196df3a985fe677b0884f76880cb59cc0d2fc
KianChesterBautista <kianchesterbautista@gmail.com>
2026-09-24 17:17:27 +0800

    Update initialize.php

--- api/initialize.php
+++ api/initialize.php
@@ -1,14 +1,16 @@
 <?php
-session_start();
+$db_host = getenv('DB_HOST');
+$db_port = getenv('DB_PORT') ?: 3306;
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+$db_name = getenv('DB_NAME');
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "lab_app";
+$db = mysqli_init();
+$db->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 
-$connection = new mysqli($host, $user, $password, $database);
-
-if($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+try {
+    $db->real_connect($db_host, $db_user, $db_pass, $db_name, $db_port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+} catch (mysqli_sql_exception $e) {
+    die("Database Connection Error: " . $e->getMessage());
 }
 ?>
