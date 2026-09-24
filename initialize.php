0bead675390fb1e0b5da4aaccbcedb437c24ea37
KianChesterBautista <kianchesterbautista@gmail.com>
2026-09-24 10:52:56 +0800

    Add files via upload

---
+++ initialize.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "lab_app";
+
+$connection = new mysqli($host, $user, $password, $database);
+
+if($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+?>
